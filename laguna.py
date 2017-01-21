ce153b431f7be8ea7540d77024ae148593923fbe
Jason Scharff <jasonscharff@gmail.com>
2017-01-21 04:49:37 -0500

    Create get_exchange_rate function

--- laguna.py
+++ laguna.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
@@ -6,6 +7,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+def get_exchange_rates(currency_code):
+    base_url = 'http://api.fixer.io/latest'
+    params = '?base={}'.format(currency_code)
+    r = requests.get(base_url + params)
+    if r.status_code == 200:
+        return r.json()['rates']
+    else:
+        return None
+
+
 
 
 if __name__ == '__main__':
